0d289b2c77359db4940800b4c83ed358ce5ba442
trungremvn <trungnguyen.remvn023@gmail.com>
2023-08-25 17:23:12 +0700

    setup auth function

---
+++ app/models/index.js
@@ -0,0 +1,36 @@
+const dbConfig = require("../config/db.config.js");
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect, // mysql
+    operatorsAliases: false,
+
+    pool: {
+        max: dbConfig.pool.max, // 5
+        min: dbConfig.pool.min, // 0
+        acquire: dbConfig.pool.acquire, // 30000
+        idle: dbConfig.pool.idle // 10000
+    }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+db.users = require("./user.model.js")(sequelize, Sequelize);
+db.roles = require("./role.model.js")(sequelize, Sequelize);
+
+db.roles.belongsToMany(db.users, {
+    through: "user_roles",
+});
+
+db.users.belongsToMany(db.roles, {
+    through: "user_roles",
+});
+
+db.ROLES = ["user", "admin", "moderator"];
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+var corsOptions = {
+    // origin: 'http://localhost:8081'
+    origin: '*'
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// root route
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to my application." });
+});
+
+require('./app/routes/turorial.routes.js')(app);
+require('./app/routes/auth.routes.js')(app);
+require('./app/routes/user.routes.js')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
+
+const db = require('./app/models/index.js');
+const Role = db.roles;
+db.sequelize.sync({ force: true }).then(() => {
+    console.log('Drop and re-sync db.');
+    initial();
+});
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: 'user'
+    });
+
+    Role.create({
+        id: 2,
+        name: 'moderator'
+    });
+
+    Role.create({
+        id: 3,
+        name: 'admin'
+    });
+};
